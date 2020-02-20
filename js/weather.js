baab42881c93fbb479dc6628760824125ebc7141
bklee2 <zenossuer@gmail.com>
2020-02-20 23:46:14 +0900

    날씨 기능 추가

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,13 @@
 const APPID = 'ad8f06e0d4b853d6bad26b776adfb233';
 
-
+// fetch 예시
 async function async_fetch(url) {
     let response = await fetch(url)
     if (response.ok) return await response.json()
     throw new Error(response.status)
 }
 
+// fetch 예시
 async function async_fetch2(urls) {
     const jsonPromises = urls.map(async url => {
         const response = await fetch(url);
@@ -19,26 +20,6 @@ async function getWeather(lat, lon) {
     const uri = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APPID}&units=metric`;
     const response = await fetch(uri).catch(error => null);
     const weather = await response.json().catch(error => null);
-   
-    // const weather = {
-    //     lat: json.coord.lat,
-    //     lon: json.coord.lon,
-    //     feels_like: json.main.feels_like,
-    //     humidity: json.main.humidity,
-    //     pressure: json.main.pressure,
-    //     temp: json.main.temp,
-    //     temp_max: json.main.temp_max,
-    //     temp_min: json.main.temp_min,
-    //     name: json.name,
-    //     country: json.sys.country,
-    //     sunrise: json.sys.sunrise,
-    //     sunset: json.sys.sunset,
-    //     description: json.weather[0].description,
-    //     icon: json.weather[0].icon,
-    //     main: json.weather[0].main,
-    //     winddeg: json.wind.deg,
-    //     windspeed: json.wind.speed,
-    // };
     return weather;
 }
 
@@ -57,23 +38,29 @@ async function getCoords() {
 }
 
 async function paintWeatherText() {
-    //location.
-    // 위치 가져오기
-    // 날씨 가져오기
-    // 텍스트 적용
     const coords = await getCoords();
     if (coords) {
         const w = await getWeather(coords.lat, coords.lon);
         if(w)
         {
-            console.dir(w);
-            console.log(w.name);
-            const text = ` @ ${w.name}, ${w.sys.country}`;
-            w.weather[0].description;
-            w.weather[0].icon;
-            w.weather[0].main;
-            w.weather[0].id;
-            const weatherText = document.querySelector('.js-weather');
+            // console.dir(w);
+            const description = w.weather[0].description;
+            // const main = w.weather[0].main;
+            // const id = w.weather[0].id;
+            const icon = w.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
+            const temp = w.main.temp;
+            // const feels_like = w.main.feels_like;
+            // const temp_max = w.main.temp_max;
+            // const temp_min = w.main.temp_min;
+            //-2˚/11˚
+            //4도씨℃ 날씨 양식 예시
+            const text = ` ${description} ${temp}℃ in ${w.name}, ${w.sys.country}`;
+            const weatherIcon = document.querySelector('#js-weather-icon');
+            weatherIcon.src = iconUrl;
+            
+            const weatherText = document.querySelector('#js-weather-text');
+            weatherText.innerHTML = text;
         }
     }
 }
