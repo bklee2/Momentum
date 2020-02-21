8a28a4fd8da10af69cb74bf57e65f7ca95de1b89
bklee2 <zenossuper@gmail.com>
2020-02-21 09:41:47 +0900

    배경 이미지 오타 수정

--- js/backgroud.js
+++
@@ -1,6 +0,0 @@
-function init() {
-    const body = document.querySelector('body');
-    body.style.backgroundImage = 'url("https://source.unsplash.com/featured/?landscape/1600")';
-}
-
-init();
---
+++ js/background.js
@@ -0,0 +1,6 @@
+function init() {
+    const body = document.querySelector('body');
+    body.style.backgroundImage = 'url("https://source.unsplash.com/featured/?landscape&w=1600")';
+}
+
+init();
